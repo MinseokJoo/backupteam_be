8c20f4c9e85ad055a21501c7f3f562e1f567d9f8
Joominseok <jumseok12@gmail.com>
2023-01-07 01:36:09 +0900

    로그아웃 부분 생성, 회원가입, 유저정보등등 대거 수정

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const jwt = require("jsonwebtoken")
 
 const users = require("./db/users")
-const articles = require("./db/articles")
+let articles = require("./db/articles")
 
 const jwtConfig = {
   secretKey : "a",
@@ -24,6 +24,10 @@ let corsOptions = {
 
 app.use(express.json(), cookieParser(),cors(corsOptions))
 
+app.get("/users", (req,res) => {
+  res.json(users)
+})
+
 app.get("/articles", (req,res) => {
   res.json(articles)
 })
@@ -32,11 +36,21 @@ app.post("/articles", (req,res) => {
   const {title, contents} = req.body
   const created_at = new Date()
 
+  if(!req.cookies.jwt) {
+    return res.json({message: "로그인 이후 글 쓰기가 가능합니다!"})
+  }
+
   const user = users.find(user => user.id === jwt.verify(req.cookies.jwt, jwtConfig.secretKey).id)
 
-  users.push({id : users.length+1,title, contents, user_id: user.id,created_at,count:0 })
+  
+  const maxObjArr = articles.reduce( (prev, value) => {
+    return prev.guildMemberCount >= value.guildMemberCount ? prev : value
+  });
+  console.log(maxObjArr.id)
 
-  res.send(users)
+  articles.push({id : maxObjArr.id + 1,title, contents, user_id: user.id,created_at,count:0 })
+
+  res.json({message: "게시글을 작성했습니다."})
 })
 
 app.get("/articles/:id", (req,res) => {
@@ -48,7 +62,7 @@ app.get("/articles/:id", (req,res) => {
     return res.json({message: "게시물 조회에 실패하였습니다."})
   }
 
-  res.send(post)
+  res.json(post)
 })
 
 app.delete("/articles/:id", (req,res) => {
@@ -69,20 +83,22 @@ app.delete("/articles/:id", (req,res) => {
     return res.status(401).json({message: "권한 업똥!"})
   }
 
-  articles[post.id - 1] = {}
+  const del_articles = articles.filter(art => art.id !== post.id)
 
-  res.send(articles)
+  articles = del_articles
+
+  res.json({message: "삭제 완료"})
 })
 
 app.put("/articles/:id", (req,res) => {
   const {id} = req.params
-  const {contents} = req.body
+  const post = articles.find(art => art.id == id)
+  console.log(post)
+  const contents = req.body.contents || post.contents
+  const title = req.body.title || post.title
   const user = users.find(user => user.id === jwt.verify(req.cookies.jwt, jwtConfig.secretKey).id)
 
-  const post = articles.find(art => art.id == id)
-
   const isIdCorrect = user.id === post.id
-
 
   if(!post) {
     return res.json({message: "게시물 조회에 실패했습니다."})
@@ -93,8 +109,9 @@ app.put("/articles/:id", (req,res) => {
   }
 
   post.contents = contents
+  post.title = title
 
-  return res.send(post)
+  return res.json({message: "수정완료;;"})
 
 })
 
@@ -102,14 +119,32 @@ app.get("/userInfos", (req,res) => {
   const userId = jwt.verify(req.cookies.jwt, jwtConfig.secretKey)
 
   const info = users.find(user => user.email === userId.email)
-  const myposts = articles.find(art => art.user_id === info.id)
+  const myposts = articles.filter(art => art.user_id === info.id)
 
   if(!info) {
     return res.status(401).json({msg: "진짜 누구세요??"})
   }
-  const mylist = Object.assign(info, myposts)
+  // const mylist = Object.assign(info, myposts)
 
-  res.json(mylist)
+  res.json({info, myposts})
+})
+
+app.post("/signup", (req,res) => {
+  const {email, password, name} = req.body
+  
+  const user = users.find(user => user.email === email)
+
+  if(user) {
+    return res.json({message: "이미 가입한 이메일 입니다."})
+  } else {
+    const maxObjArr = users.reduce( (prev, value) => {
+      return prev.guildMemberCount >= value.guildMemberCount ? prev : value
+    });
+    console.log(maxObjArr.id)
+
+    users.push({id: maxObjArr.id + 1, name, email, password, created_at: new Date()})
+    return res.json({message: "회원가입 축하다"})
+  }
 })
 
 app.post("/login", (req,res) => {
@@ -122,8 +157,12 @@ app.post("/login", (req,res) => {
   }
 
   res.cookie("jwt", jwt.sign(user, jwtConfig.secretKey, jwtConfig.options))
-  console.log(req.cookies)
   res.status(200).json({msg:`${user.name}님 환영합니다`})
+})
+
+app.get("/logout", (req,res) => {
+  res.clearCookie("jwt")
+  res.end()
 })
 
 app.listen(5000,() => console.log(5000,"번 서버 열림"))
